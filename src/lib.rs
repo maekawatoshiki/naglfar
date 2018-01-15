766549285bf0ea2057d2c1bd39c88e1a752a2d04
uint256_t <konndennsa@gmail.com>
2018-01-15 18:54:45 +0900

    kinda figure out html5ever

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod renderer;
+
+#[macro_use]
+extern crate html5ever;
+extern crate tendril;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+use clap::{App, Arg};
+
+extern crate naglfar;
+use naglfar::renderer;
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("naglfar");
+    let app = App::new("naglfar")
+        .version(VERSION_STR)
+        .author("uint256_t")
+        .about("naglfar is a web browser implementation in Rust")
+        .arg(Arg::with_name("FILE").help("Input file").index(1));
+    let _app_matches = app.get_matches();
+    renderer::f();
 }
