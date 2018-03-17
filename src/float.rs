535c9e71494ea44c379c38534db0122c5ed65fe6
maekawatoshiki <konndennsa@gmail.com>
2018-03-17 16:37:28 +0900

    refine code

--- src/float.rs
+++ src/float.rs
@@ -1,5 +1,6 @@
-use layout::{EdgeSizes, Rect};
+use layout::{EdgeSizes, LayoutBox, Rect};
 use style;
+use css::{Unit, Value};
 
 use app_units::Au;
 
@@ -112,5 +113,57 @@ impl Floats {
                 _ => Au(0),
             }
         }) - self.offset.right
+    }
+}
+
+impl<'a> LayoutBox<'a> {
+    /// Calculate the width of a float (non-replaced) element.
+    /// Sets the horizontal margin/padding/border dimensions, and the `width`.
+    /// ref. https://www.w3.org/TR/2007/CR-CSS21-20070719/visudet.html#float-width
+    // TODO: Implement correctly!
+    pub fn calculate_float_width(&mut self) {
+        let style = self.get_style_node();
+
+        // `width` has initial value `auto`.
+        let auto = Value::Keyword("auto".to_string());
+        let width = style.value("width").unwrap_or(auto.clone());
+
+        // margin, border, and padding have initial value 0.
+        let zero = Value::Length(0.0, Unit::Px);
+
+        let margin_left = style.lookup("margin-left", "margin", &zero);
+        let margin_right = style.lookup("margin-right", "margin", &zero);
+
+        let border_left = style.lookup("border-left-width", "border-width", &zero);
+        let border_right = style.lookup("border-right-width", "border-width", &zero);
+
+        let padding_left = style.lookup("padding-left", "padding", &zero);
+        let padding_right = style.lookup("padding-right", "padding", &zero);
+
+        let d = &mut self.dimensions;
+        if let Some(width) = width.to_px() {
+            d.content.width = Au::from_f64_px(width)
+        }
+
+        if let Some(padding_left) = padding_left.to_px() {
+            d.padding.left = Au::from_f64_px(padding_left)
+        }
+        if let Some(padding_right) = padding_right.to_px() {
+            d.padding.right = Au::from_f64_px(padding_right)
+        }
+
+        if let Some(border_left) = border_left.to_px() {
+            d.border.left = Au::from_f64_px(border_left)
+        }
+        if let Some(border_right) = border_right.to_px() {
+            d.border.right = Au::from_f64_px(border_right)
+        }
+
+        if let Some(margin_left) = margin_left.to_px() {
+            d.margin.left = Au::from_f64_px(margin_left)
+        }
+        if let Some(margin_right) = margin_right.to_px() {
+            d.margin.right = Au::from_f64_px(margin_right)
+        }
     }
 }
