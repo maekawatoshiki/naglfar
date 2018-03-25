96104661e4170cf979cc127aae4b3f94797e86e9
maekawatoshiki <konndennsa@gmail.com>
2018-03-25 23:30:38 +0900

    can run specified file

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,13 @@ fn main() {
         .author("uint256_t")
         .about("Naglfar is a web browser implementation in Rust")
         .arg(Arg::with_name("FILE").help("Input file").index(1));
-    let _app_matches = app.get_matches();
+    let app_matches = app.get_matches();
 
-    run_with_url("./example/test.html".to_string());
+    run_with_url(if let Some(filename) = app_matches.value_of("FILE") {
+        filename.to_string()
+    } else {
+        "./example/test.html".to_string()
+    });
 }
 
 fn run_with_url(html_src: String) {
