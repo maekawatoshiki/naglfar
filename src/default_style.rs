4149318259a8e4367be5751450d42c56618bb2e3
maekawatoshiki <konndennsa@gmail.com>
2018-04-19 22:43:15 +0900

    add new default style rules

--- src/default_style.rs
+++ src/default_style.rs
@@ -1,15 +1,44 @@
 use css::*;
 
 use std::collections::HashSet;
+use std::cell::RefCell;
 
 pub fn default_rules() -> Vec<Rule> {
-    let mut rules = vec![];
-    rule_universal(&mut rules);
-    rule_html(&mut rules);
-    rule_body(&mut rules);
-    rule_span(&mut rules);
-    rules
+    DEFAULT_RULES.with(|default_rules| default_rules.borrow().clone())
 }
+
+thread_local!(
+    pub static DEFAULT_RULES: RefCell<Vec<Rule>> = {
+        let mut rules = vec![];
+        rule_universal(&mut rules);
+        rule_html(&mut rules);
+        rule_body(&mut rules);
+        rule_span(&mut rules);
+        rule_h1(&mut rules);
+        rule_h2(&mut rules);
+        rule_h3(&mut rules);
+        rule_a(&mut rules);
+        rule_img(&mut rules);
+        RefCell::new(rules)
+    }
+);
+
+macro_rules! tag_name { ($name:expr) => {
+    Selector::Simple(SimpleSelector {
+        tag_name: Some($name.to_string()), id: None, class: HashSet::new() })
+}}
+
+macro_rules! decl { ($name:expr, $( $val:expr ),*) => {
+    Declaration {
+        name: $name.to_string(),
+        values: vec![$($val)*],
+    }
+}}
+
+macro_rules! keyword { ($str:expr) => { Value::Keyword($str.to_string()) }}
+macro_rules! len_px  { ($val:expr) => { Value::Length($val, Unit::Px) }}
+// macro_rules! num     { ($val:expr) => { Value::Num($val) }}
+macro_rules! color   { ($clr:expr) => { Value::Color($clr) }}
 
 fn rule_universal(rules: &mut Vec<Rule>) {
     rules.push(Rule {
@@ -20,81 +49,94 @@ fn rule_universal(rules: &mut Vec<Rule>) {
                 class: HashSet::new(),
             }),
         ],
-        declarations: vec![
-            Declaration {
-                name: "display".to_string(),
-                values: vec![Value::Keyword("block".to_string())],
-            },
-        ],
+        declarations: vec![decl!("display", keyword!("block"))],
     });
 }
 
 fn rule_html(rules: &mut Vec<Rule>) {
     rules.push(Rule {
-        selectors: vec![
-            Selector::Simple(SimpleSelector {
-                tag_name: Some("html".to_string()),
-                id: None,
-                class: HashSet::new(),
-            }),
-        ],
+        selectors: vec![tag_name!("html")],
         declarations: vec![
-            Declaration {
-                name: "width".to_string(),
-                values: vec![Value::Keyword("auto".to_string())],
-            },
-            Declaration {
-                name: "padding".to_string(),
-                values: vec![Value::Length(0f64, Unit::Px)],
-            },
-            Declaration {
-                name: "margin".to_string(),
-                values: vec![Value::Length(0f64, Unit::Px)],
-            },
-            Declaration {
-                name: "background".to_string(),
-                values: vec![Value::Color(WHITE)],
-            },
+            decl!("width", keyword!("auto")),
+            decl!("padding", len_px!(0f64)),
+            decl!("margin", len_px!(0f64)),
+            decl!("background", color!(WHITE)),
         ],
     });
 }
 
 fn rule_body(rules: &mut Vec<Rule>) {
     rules.push(Rule {
-        selectors: vec![
-            Selector::Simple(SimpleSelector {
-                tag_name: Some("body".to_string()),
-                id: None,
-                class: HashSet::new(),
-            }),
-        ],
+        selectors: vec![tag_name!("body")],
         declarations: vec![
-            Declaration {
-                name: "padding".to_string(),
-                values: vec![Value::Length(0f64, Unit::Px)],
-            },
-            Declaration {
-                name: "margin".to_string(),
-                values: vec![Value::Length(0f64, Unit::Px)],
-            },
+            decl!("padding", len_px!(0f64)),
+            decl!("margin", len_px!(0f64)),
         ],
     });
 }
 
 fn rule_span(rules: &mut Vec<Rule>) {
     rules.push(Rule {
-        selectors: vec![
-            Selector::Simple(SimpleSelector {
-                tag_name: Some("span".to_string()),
-                id: None,
-                class: HashSet::new(),
-            }),
-        ],
+        selectors: vec![tag_name!("span")],
+        declarations: vec![decl!("display", keyword!("inline"))],
+    });
+}
+
+fn rule_h1(rules: &mut Vec<Rule>) {
+    rules.push(Rule {
+        selectors: vec![tag_name!("h1")],
         declarations: vec![
-            Declaration {
-                name: "display".to_string(),
-                values: vec![Value::Keyword("inline".to_string())],
-            },
+            decl!("font-size", len_px!(30f64)),
+            decl!("font-weight", keyword!("bold")),
+            decl!("padding", len_px!(10f64)),
         ],
+    });
+}
+
+fn rule_h2(rules: &mut Vec<Rule>) {
+    rules.push(Rule {
+        selectors: vec![tag_name!("h2")],
+        declarations: vec![
+            decl!("font-size", len_px!(24f64)),
+            decl!("font-weight", keyword!("bold")),
+            decl!("padding", len_px!(10f64)),
+        ],
+    });
+}
+
+fn rule_h3(rules: &mut Vec<Rule>) {
+    rules.push(Rule {
+        selectors: vec![tag_name!("h3")],
+        declarations: vec![
+            decl!("font-size", len_px!(19f64)),
+            decl!("font-weight", keyword!("bold")),
+            decl!("padding", len_px!(10f64)),
+        ],
+    });
+}
+
+fn rule_a(rules: &mut Vec<Rule>) {
+    rules.push(Rule {
+        selectors: vec![tag_name!("a")],
+        declarations: vec![
+            decl!("display", keyword!("inline")),
+            decl!(
+                "color",
+                color!(Color {
+                    r: 0,
+                    g: 0,
+                    b: 0xee,
+                    a: 0xff,
+                })
+            ),
+            decl!("text-decoration", keyword!("underline")),
+        ],
+    });
+}
+
+fn rule_img(rules: &mut Vec<Rule>) {
+    rules.push(Rule {
+        selectors: vec![tag_name!("img")],
+        declarations: vec![decl!("display", keyword!("inline"))],
     });
 }
